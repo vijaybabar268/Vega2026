f8e6944a9fcac89432925c839a6144d1b529fb09
Vijaybabar <babarvm@gmail.com>
2026-01-27 23:22:08 +0530

    Refactor code

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure the HTTP request pipeline.
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
