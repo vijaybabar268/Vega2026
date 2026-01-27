bb4e34aaf3d425d76c1f61beb468632f47c039a6
Vijaybabar <babarvm@gmail.com>
2026-01-27 23:29:47 +0530

    Buiilding a domain models

---
+++ API/Models/Model.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace API.Models;
+
+public class Model
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public int MakeId { get; set; }
+    public Make Make { get; set; }
+}
